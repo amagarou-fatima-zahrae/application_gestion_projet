78d1741be8f08ee0ab22a48fa661198908b43c09
Sefiane-Ouami <73316945+Sefiane-Ouami@users.noreply.github.com>
2022-10-31 23:18:10 +0100

    project architecture

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+@extends('template.masterPage')
+
+@section('title')
+    <title>Home Page</title>
+@endsection
+
+@section('content')
+<section id="home" class="home">
+    <div class="container d-flex justify-content-center ">
+      <h1 class="text m-3"> This is only a Test Page</h1>
+    </div>
+</section>
+@endsection
